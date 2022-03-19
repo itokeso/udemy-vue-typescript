9aa8e04579db8287d7c4f1579e1b7310fadc57ab
itokeso <headwaybasket@yahoo.co.jp>
2022-03-19 17:34:48 +0900

    作成

--- vite-app/src/components/PersonPostForm.vue
+++ vite-app/src/components/PersonPostForm.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+const inputtingName = ref<string>('')
+const inputtingAge = ref<number>(0)
+
+const emit = defineEmits(['register'])
+
+const register = () => {
+    const person = {id: Math.random(), name: inputtingName.value, age: inputtingAge.value}
+    emit('register', person)
+}
 </script>
 
 <template>
     <div class="form-container">
         <div class="input-container">
-            <div>
+            <div class="input-column">
                 <span>name:</span>
-                <input class="input" />
+                <input class="input"  v-model="inputtingName" />
             </div>
-            <div>
+            <div class="input-column">
                 <span>age:</span>
-                <input class="input" />
+                <input class="input" v-model="inputtingAge" type="number" />
             </div>
         </div>
-        <button class="register-button">register</button>
+        <button @click="register" class="register-button">register</button>
     </div>
 </template>
 
@@ -30,6 +40,16 @@
     border-radius: 12px;
 }
 
+.input-column {
+    width: 200px;
+    display: flex;
+    justify-content: space-between;
+}
+
+input {
+    width: 120px;
+    margin-bottom: 8px;
+}
 .input-container {
     display: flex;
     flex-direction: column;
--- vite-app/src/components/Persons.vue
+++ vite-app/src/components/Persons.vue
@@ -1,15 +1,31 @@
 <script setup lang="ts">
 import PersonPostForm from './PersonPostForm.vue';
 import PersonList from './PersonList.vue';
+import { ref, Ref} from 'vue';
+
+export type Person = {
+    id: number,
+    name: string,
+    age: number,
+}
+
+const persons: Ref<Person[]> = ref([{id: 0, name: 'Jon', age: 24}, {id: 1, name: 'mike', age: 30} ])
+const registerPerson = (person: Person) => {
+    persons.value.push(person)
+}
+
+const deletePerson = (id: number) => {
+    persons.value = persons.value.filter(p => p.id !== id)
+}
 </script>
 
 <template>
     <div class="container">
         <h1>TITLE</h1>
-        <PersonPostForm />
+        <PersonPostForm @register="registerPerson" />
         <div class="list-container">
             <ul>
-                <PersonList />
+                <PersonList @delete="deletePerson" :persons="persons" />
             </ul>
         </div> 
     </div>
