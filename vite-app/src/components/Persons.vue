27361e2b1e95a2784561e241a8b52dfc294e7964
itokeso <headwaybasket@yahoo.co.jp>
2022-03-19 16:02:52 +0900

    コンポーネントを作成

---
+++ vite-app/src/components/Persons.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import PersonPostForm from './PersonPostForm.vue';
+import PersonList from './PersonList.vue';
+</script>
+
+<template>
+    <div class="container">
+        <h1>TITLE</h1>
+        <PersonPostForm />
+        <div class="list-container">
+            <ul>
+                <PersonList />
+            </ul>
+        </div> 
+    </div>
+</template>
+
+<style scoped>
+
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
